7e6ac52a33c11da06ca3dcf05fc26a79eab06c0c
Areej <areejahmed.27502@gmail.com>
2024-08-25 16:38:44 +0300

    Update MainContent.jsx

--- alx-react-app-new/src/components/MainContent.jsx
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -2,7 +2,12 @@
 function MainContent(){
     return(
         <main>
-            <p>I love to visit New York, Paris, and Tokyo.</p>
+            <p style = {{ border: '3px' solid black'}}>I love to visit New York, Paris, and Tokyo.</p>
+             <div style={{ border: '1px solid gray', padding: '10px', margin: '10px' }}>
+   <h2 style={{ color: 'blue' }}>{props.name}</h2>
+   <p>Age: <span style={{ fontWeight: 'bold' }}>{props.age}</span></p>
+   <p>Bio: {props.bio}</p>
+ </div>
             </main>
     );
 }
