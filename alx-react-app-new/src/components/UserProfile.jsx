5a69723d596175ceb0fc9c32f78b1b643e519ced
Areej <areejahmed.27502@gmail.com>
2024-08-25 16:35:18 +0300

    Update UserProfile.jsx

--- alx-react-app-new/src/components/UserProfile.jsx
+++ alx-react-app-new/src/components/UserProfile.jsx
@@ -2,11 +2,12 @@
 function UserProfile({name, age, bio}){
     const UserProfile = (props) => {
         return (
-          <div>
-            <h2>{props.name}</h2>
-            <p>Age: {props.age}</p>
-            <p>Bio: {props.bio}</p>
-          </div>
+            
+            <div style={{ border: '1px solid gray', padding: '10px', margin: '7px' }}>
+                <h2 style={{ color: 'blue'}}>{props.name}</h2>
+                <p>Age: <span style={{ fontWeight: 'bold' }}>{props.age}</span></p>
+                <p>Bio: {props.bio}</p>
+            </div>
         );
       };
 }
