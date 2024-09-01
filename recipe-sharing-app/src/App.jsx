1fea1a1a15c39319ec5426d06ee9055350dbe176
Areej <areejahmed.27502@gmail.com>
2024-09-01 21:52:01 +0300

    Update App.jsx

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -1,11 +1,24 @@
 import RecipeList from "./components/RecipeList";
 import AddRecipeForm from "./components/AddRecipeForm";
+import RecipeDetails from "./components/RecipeDetails";
+import DeleteRecipeButton from "./components/DeleteRecipeButton";
+import EditRecipeForm from "./components/EditRecipeForm";
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 function App() {
   return(
     <>
-      <RecipeList />
-      <AddRecipeForm />
+
+<BrowserRouter>
+      <Routes>
+        <Route path="/" element= {<RecipeList />} />
+        <Route path="/recipes/:recipeId" element = {<RecipeDetails />} />
+        <Route path = "AddRecipeForm" element = {<AddRecipeForm />} />
+        <Route path = "RecipeDetails" element = {<RecipeDetails />} />
+        <Route path = "DeleteRecipeButton" element = {<DeleteRecipeButton />} />
+        <Route path = "EditRecipeForm" element = {<EditRecipeForm />} />
+      </Routes>
+    </BrowserRouter>
     </>
   );
 }
